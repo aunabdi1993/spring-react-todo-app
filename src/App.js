38aa7f33d8055dd3af66276e3cbb86af02c1a5eb
aun.abdi <aun.abdi@webexpenses.com>
2023-02-11 22:34:51 +0000

    add full list and map

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route path="/welcome/:username" element={<Welcome />} />
+                    <Route path="/todos" element={<Todos />} />
                     <Route path="*" element={<Error />} />
                 </Routes>
             </BrowserRouter>
@@ -134,7 +135,7 @@ function App() {
 
     function Welcome() {
 
-        const {username} = useParams();
+        const { username } = useParams();
         console.log(username);
 
         return (
@@ -158,6 +159,68 @@ function App() {
                         <h2 className="mt-3 text-center text-xl font-bold tracking-tight text-gray-900">
                             Error. Please return to Login page.
                         </h2>
+                    </div>
+                </div>
+            </div>
+        )
+    };
+
+    function Todos() {
+
+        const today = new Date();
+        const targetDate = today.getFullYear()+12 + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+        const todos = [
+            { id: 1, description: 'Learn React', done: false, targetDate: targetDate },
+            { id: 2, description: 'Learn Java', done: false, targetDate: targetDate },
+            { id: 3, description: 'Learn Python', done: false, targetDate: targetDate },
+            { id: 4, description: 'Learn C++', done: false, targetDate: targetDate },
+            { id: 5, description: 'Learn C#', done: false, targetDate: targetDate },
+        ]
+
+        return (
+            <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+                <div className="w-full max-w-md space-y-8">
+                    <div>
+                        <h1 className="mt-3 text-center text-xl font-bold tracking-tight text-gray-900">
+                            Things you want to do !
+                        </h1>
+                        <div>
+                            <>
+                                <div className="mt-2 flex flex-col">
+                                    <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
+                                        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                                <table className="min-w-full divide-y divide-gray-200">
+                                                    <thead className="bg-gray-50">
+                                                        <tr>
+                                                            <td>ID</td>
+                                                            <td>Description</td>
+                                                            <td>Done</td>
+                                                            <td>Target Date</td>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody
+                                                        className="bg-white divide-y divide-gray-200"
+                                                    >
+                                                        {
+                                                            todos.map( todo => (
+                                                                <tr key={todo.id}>
+                                                                    <td>{todo.id}</td>
+                                                                    <td>{todo.description}</td>
+                                                                    <td>{todo.done.toString()}</td>
+                                                                    <td>{todo.targetDate}</td>
+                                                                </tr>
+                                                            ))
+                                                        }
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        </div>
                     </div>
                 </div>
             </div>
